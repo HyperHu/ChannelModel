e0887c8c2afb3ba15c3cf8e6d158f4abf9ff4b11
Hyper <hyper_hu@icloud.com>
2019-09-23 22:49:34 +0800

    On branch master: learning at weekend.

--- Demo/Demo_SISO_OneRay.m
+++ Demo/Demo_SISO_OneRay.m
@@ -26,7 +26,7 @@ nData = numPRB * 12;
 % aoa_deg\zoa_deg:
 
 %theRay = genRandomRay(1, 0, (4/sampleRate)*1e9, 1, 0);
-theRay = [0, (15/sampleRate)*1e9, subCarriarSpace/2, 0];
+theRay = [0, (5/sampleRate)*1e9, subCarriarSpace/64, 0];
 
 %% calculate H[k,l], it is ideal, means no noise, no ICI.
 theChannel_ideal = calChannelH(theRay, 0, mu, nFFT, simulationTimeMs);
@@ -36,19 +36,19 @@ theChannel_ideal = calChannelH(theRay, 0, mu, nFFT, simulationTimeMs);
 %% calculate ICI[k,l]
 nSym = size(theChannel_ideal,2);
 [theICI, v, sig] = genRicianIci(nFFT, nSym, theRay(1,1), theRay(1,3)/subCarriarSpace);
-%figure(1); mesh(abs(theICI));
-%figure(2); mesh(angle(theICI));
-%figure(3); grid on; histfit(reshape(abs(theICI), 1, []), 100, 'rician');
+figure(1); mesh(abs(theICI));
+figure(2); mesh(angle(theICI));
+figure(3); grid on; histfit(reshape(abs(theICI), 1, []), 100, 'rician');
 %%
 %pdICI = fitdist(reshape(abs(theICI), [], 1), 'rician')
 %tttH = fft2(theChannel_ideal(:,1:256) + theICI(:,1:256));
 %mesh(abs(fftshift(tttH)));
 
-figure(); hold on; grid on;
-tttH = fft(theChannel_ideal(10,1:256) + theICI(10,1:256));
-plot(abs(fftshift(tttH)),'*--');
-tttH = fft(theChannel_ideal(100,1:256) + theICI(100,1:256));
-plot(abs(fftshift(tttH)),'o--');
+% figure(); hold on; grid on;
+% tttH = fft(theChannel_ideal(10,1:256) + theICI(10,1:256));
+% plot(abs(fftshift(tttH)),'*--');
+% tttH = fft(theChannel_ideal(100,1:256) + theICI(100,1:256));
+% plot(abs(fftshift(tttH)),'o--');
 
 %%
 %pdH = fitdist(reshape(real(theChannel_ideal), [], 1), 'normal');
--- Demo/IciDistribution.m
+++ Demo/IciDistribution.m
@@ -5,47 +5,43 @@ addpath(pwd + "\..");
 addpath(pwd + "\..\Utility");
 
 modulateOrder = 2;
-nFFT = 2048;
-nSymbol = 14 * 100;
+nFFT = 512;
+nSymbol = 14 * 500;
 
 %%
-kdsList = 0.01:0.05:0.5;
-resultS = zeros(size(kdsList));
-resultSigma = zeros(size(kdsList));
-for idxI = 1:size(kdsList,2)
-    idxI
-    [tmpS, tmpSigma] = calRicianPara(nFFT, nSymbol, kdsList(idxI), modulateOrder);
-    resultS(idxI) = tmpS;
-    resultSigma(idxI) = tmpSigma * sqrt(2);
-end
-
-figure(1); hold on; grid on;
-plot(kdsList, resultS, '*--');
-plot(kdsList, resultSigma, 'o--');
-%plot(kdsList, sqrt(resultS .^ 2 + 2*(resultSigma .^ 2)));
+% kdsList = 0.01:0.05:0.5;
+% resultS = zeros(size(kdsList));
+% resultSigma = zeros(size(kdsList));
+% for idxI = 1:size(kdsList,2)
+%     idxI
+%     [tmpS, tmpSigma] = calRicianPara(nFFT, nSymbol, kdsList(idxI), modulateOrder);
+%     resultS(idxI) = tmpS;
+%     resultSigma(idxI) = tmpSigma * sqrt(2);
+% end
+% 
+% figure(1); hold on; grid on;
+% plot(kdsList, resultS, '*--');
+% plot(kdsList, resultSigma, 'o--');
+% %plot(kdsList, sqrt(resultS .^ 2 + 2*(resultSigma .^ 2)));
 
 
 %% random data and do statistic
-kds = 0.1;
+kds = 0.5/8;
 sumIci = zeros(nFFT, nSymbol);
-[reMatPilot,~] = genRandomREValue(nFFT, nSymbol, 2, 1);
-
 for idxD = 1:24
     [reMatData,~] = genRandomREValue(nFFT, nSymbol, modulateOrder, 1);
-    tmpI = reMatData ./ reMatPilot;
-    tmpP = calPhaseFactor(nFFT, zeros(1,nSymbol), 0, 0, kds, idxD);
+    tmpP = calPhaseFactor(nFFT, zeros(1,nSymbol), 0, 8, kds, idxD);
     tmpIci = calIciFactor(idxD, kds, nFFT);
-    sumIci = sumIci + tmpI .* tmpP .* tmpIci;
+    sumIci = sumIci + reMatData .* tmpP .* tmpIci;
     [reMatData,~] = genRandomREValue(nFFT, nSymbol, modulateOrder, 1);
-    tmpI = reMatData ./ reMatPilot;
-    tmpP = calPhaseFactor(nFFT, zeros(1,nSymbol), 0, 0, kds, -idxD);
+    tmpP = calPhaseFactor(nFFT, zeros(1,nSymbol), 0, 8, kds, -idxD);
     tmpIci = calIciFactor(-idxD, kds, nFFT);
-    sumIci = sumIci + tmpI .* tmpP .* tmpIci;
+    sumIci = sumIci + reMatData .* tmpP .* tmpIci;
 end
 
 figure(1); hold on; grid on;
-histogram(reshape(real(sumIci), 1, []), 100);
-histogram(reshape(imag(sumIci), 1, []), 100);
+histfit(reshape(real(sumIci), 1, []), 100);
+histfit(reshape(imag(sumIci), 1, []), 100);
 figure(2); hold on; grid on;
 histfit(reshape(abs(sumIci), 1, []), 100, 'rician');
 
@@ -55,12 +51,12 @@ sumIci = zeros(nFFT, nSymbol);
 for idxD = 1:36
     [reMatData,~] = genRandomREValue(nFFT, nSymbol, modulateOrder, 1);
     tmpI = reMatData ./ reMatPilot;
-    tmpP = calPhaseFactor(nFFT, zeros(1,nSymbol), 0, 10, kds, idxD);
+    tmpP = calPhaseFactor(nFFT, zeros(1,nSymbol), 0, 30, kds, idxD);
     tmpIci = calIciFactor(idxD, kds, nFFT);
     sumIci = sumIci + tmpI .* tmpP .* tmpIci;
     [reMatData,~] = genRandomREValue(nFFT, nSymbol, modulateOrder, 1);
     tmpI = reMatData ./ reMatPilot;
-    tmpP = calPhaseFactor(nFFT, zeros(1,nSymbol), 0, 10, kds, -idxD);
+    tmpP = calPhaseFactor(nFFT, zeros(1,nSymbol), 0, 30, kds, -idxD);
     tmpIci = calIciFactor(-idxD, kds, nFFT);
     sumIci = sumIci + tmpI .* tmpP .* tmpIci;
 end
