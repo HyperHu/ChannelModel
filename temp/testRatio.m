824d5e9b62eac1ea83be9c971ea6f28c6bb54645
Hyper <hyper_hu@icloud.com>
2019-09-26 11:15:51 +0800

    On branch master: demo for channel capacity

---
+++ temp/testRatio.m
@@ -0,0 +1,37 @@
+clear all;
+
+%%
+% nList = 1:10;
+% wList = 1:20;
+% tmpRatio = zeros(size(nList,2), size(wList,2));
+% 
+% for idxN = 1:size(nList,2)
+%     for idxW = 1:size(wList,2)
+%         n = nList(idxN);
+%         w = wList(idxW);
+%         tmpRatio(idxN, idxW) = ((w*n/(w*n+1)) + (w /(w+n))) / ((1/(w*n+1)) + (n/(w+n)));
+%     end
+% end
+% 
+% mesh(wList, nList, tmpRatio);
+
+%%
+xdBList = -20:0.1:20;
+xList = db2pow(xdBList);
+tmpV1 = log2(1 + xList);
+tmpV2 = log2(xList);
+tmpV3 = xList .* log2(exp(1));
+
+figure(1); hold on; grid on;
+plot(xdBList, tmpV1);
+plot(xdBList(200:end), tmpV2(200:end), '--');
+plot(xdBList(1:200), tmpV3(1:200), '.');
+
+figure(2); hold on; grid on;
+plot(xdBList(200:end), (tmpV2(200:end) - tmpV1(200:end)) ./ tmpV1(200:end));
+plot(xdBList(1:200), (tmpV3(1:200) - tmpV1(1:200)) ./ tmpV1(1:200));
+plot(xdBList, 0.05 * ones(1,size(xdBList,2)));
+plot(xdBList, -0.05 * ones(1,size(xdBList,2)));
+
+
+
