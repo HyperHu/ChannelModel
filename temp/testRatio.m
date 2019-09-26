48f8a53028ba65ff27fe66d67b26ecb5abaa16d2
Hyper <hyper_hu@icloud.com>
2019-09-26 18:59:47 +0800

    On branch master: middle sinr

--- temp/testRatio.m
+++ temp/testRatio.m
@@ -21,15 +21,20 @@ xList = db2pow(xdBList);
 tmpV1 = log2(1 + xList);
 tmpV2 = log2(xList);
 tmpV3 = xList .* log2(exp(1));
+tmpV4 = (0.08*xdBList + 1.02) .^ 2;
+%tmpV4 = (0.09*xdBList) .^ 2 + (0.17 * xdBList) + 1;
 
 figure(1); hold on; grid on;
 plot(xdBList, tmpV1);
-plot(xdBList(200:end), tmpV2(200:end), '--');
-plot(xdBList(1:200), tmpV3(1:200), '.');
+plot(xdBList(200:end), tmpV2(200:end));
+plot(xdBList(1:200), tmpV3(1:200));
+plot(xdBList(100:300), tmpV4(100:300));
+
 
 figure(2); hold on; grid on;
 plot(xdBList(200:end), (tmpV2(200:end) - tmpV1(200:end)) ./ tmpV1(200:end));
 plot(xdBList(1:200), (tmpV3(1:200) - tmpV1(1:200)) ./ tmpV1(1:200));
+plot(xdBList(100:300), (tmpV4(100:300) - tmpV1(100:300)) ./ tmpV1(100:300));
 plot(xdBList, 0.05 * ones(1,size(xdBList,2)));
 plot(xdBList, -0.05 * ones(1,size(xdBList,2)));
 
